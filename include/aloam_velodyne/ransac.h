adb648259bfd677f1baae43a4f063cffd026f3b3
Renlang Huang <55647545+RenlangHuang@users.noreply.github.com>
2023-07-14 20:52:03 +0800

    Update ransac.h

--- include/aloam_velodyne/ransac.h
+++ include/aloam_velodyne/ransac.h
@@ -2,50 +2,43 @@
 
 #include <cmath>
 #include <open3d/Open3D.h>
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
-#include <pcl/kdtree/kdtree_flann.h>
 
-using namespace open3d;
+namespace open3d {
+namespace pipelines {
+namespace registration {
 
-
-void convert_o3d_to_pcl_XYZ(
-    const geometry::PointCloud &ply,
-    const pcl::PointCloud<pcl::PointXYZ>::Ptr pcd
+CorrespondenceSet LocalFeatureMatching(
+    const geometry::KDTreeFlann &kdtree,
+    const geometry::PointCloud &target,
+    const Feature &source_features,
+    const Feature &target_features,
+    double max_correspondence_distance,
+    const int num_similar_features,
+    const double similarity_threshold,
+    const bool reverse
 );
 
-
-pipelines::registration::RegistrationResult RANSACWithInitialPruning(
+RegistrationResult RANSACWithInitialPruning(
     const geometry::PointCloud &source,
     const geometry::PointCloud &target,
-    const pipelines::registration::Feature &source_feature,
-    const pipelines::registration::Feature &target_feature,
-    double max_correspondence_distance,
-    int ransac_n,
-    const std::vector<std::reference_wrapper<const pipelines::registration::CorrespondenceChecker>> &checkers,
-    const pipelines::registration::RANSACConvergenceCriteria &criteria
+    const Feature &source_feature,
+    const Feature &target_feature,
+    const double max_pruning_distance, double max_correspondence_distance,
+    int ransac_n, const int num_similar_features, const double similarity_threshold,
+    const std::vector<std::reference_wrapper<const CorrespondenceChecker>> &checkers,
+    const RANSACConvergenceCriteria &criteria
 );
 
-
-pipelines::registration::RegistrationResult RANSACFromLocalToGlobal(
+RegistrationResult RegistrationFromLocalToGlobal(
     const geometry::PointCloud &source,
     const geometry::PointCloud &target,
-    const pipelines::registration::Feature &source_feature,
-    const pipelines::registration::Feature &target_feature,
-    double max_correspondence_distance,
-    const std::vector<std::reference_wrapper<const pipelines::registration::CorrespondenceChecker>> &checkers,
-    const pipelines::registration::RANSACConvergenceCriteria &criteria
+    const Feature &source_feature,
+    const Feature &target_feature,
+    double max_pruning_distance, double max_correspondence_distance,
+    int num_similar_features, double similarity_threshold, double decay,
+    const ICPConvergenceCriteria &criteria
 );
 
-
-pipelines::registration::RegistrationResult RANSAC(
-        const geometry::PointCloud &source,
-        const geometry::PointCloud &target,
-        const pipelines::registration::Feature &source_feature,
-        const pipelines::registration::Feature &target_feature,
-        double max_correspondence_distance,
-        const pipelines::registration::TransformationEstimation &estimation,
-        int ransac_n,
-        const std::vector<std::reference_wrapper<const pipelines::registration::CorrespondenceChecker>> &checkers,
-        const pipelines::registration::RANSACConvergenceCriteria &criteria
-);
+}  // namespace registration
+}  // namespace pipelines
+}  // namespace open3d
